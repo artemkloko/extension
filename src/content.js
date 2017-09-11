85829b1c8cc39463f44dba5b25db0cfb11f5e365
Artem Kloko <artemkloko@gmail.com>
2017-09-12 01:19:39 +0200

    progresssss

--- src/content.js
+++ src/content.js
@@ -55,7 +55,6 @@ var observer = new MutationObserver(function(mutations) {
 });
 
 
-
 observer.observe( document.body, {
     childList: true,
     subtree: true,
@@ -117,96 +116,59 @@ var decrypt = function ( encrypted, callback )
 };
 
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-
-
-        if ( typeof msg.encrypt === 'string' )
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) 
+{
+    if ( typeof msg.encrypt === 'string' )
+    {
+        encrypt( msg.encrypt, function ( encrypted ) 
         {
-            encrypt( msg.encrypt, function ( encrypted ) 
-            {
-                console.log( 'encrypted', encrypted );
-                // var el = document.querySelector('div[contenteditable="true"] span[data-text="true"]');
-                var el = document.querySelector('div[contenteditable="true"]');
-                el.addEventListener('input', function (e)
-                    {
-                        console.log(e);
-                    });
-                // el.innerHTML = encrypted;
-
-
-                // var keyboardEvent = document.createEvent("KeyboardEvent");
-                // var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-
-
-                // keyboardEvent[initMethod](
-                //                    "keydown", // event type : keydown, keyup, keypress
-                //                     true, // bubbles
-                //                     true, // cancelable
-                //                     window, // viewArg: should be window
-                //                     false, // ctrlKeyArg
-                //                     false, // altKeyArg
-                //                     false, // shiftKeyArg
-                //                     false, // metaKeyArg
-                //                      // keyCodeArg : unsigned long the virtual key code, else 0
-                //                     0, "e".charCodeAt(0) // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-                // );
-                // document.dispatchEvent(keyboardEvent);
-
-
-
-                // var allElements = document.querySelectorAll( 'a' );
-                // for (var i = 0; i < allElements.length; i++) {
-                //     if ( allElements[i].innerHTML === 'Send' ) {
-                //         allElements[i].click();
-                //     }
-                // }
-
-            } );
-        } 
-        else if ( typeof msg.decrypt === 'string' )
+            post( encrypted );
+        } );
+    } 
+    else if ( typeof msg.decrypt === 'string' )
+    {
+        decrypt( msg.decrypt, function ( decrypted ) 
         {
-            decrypt( msg.decrypt, function ( decrypted ) 
-            {
-                sendResponse(decrypted);
-            } );
-        }  
+            sendResponse(decrypted);
+        } );
+    }  
 });
 
 
+var post = function ( text )
+{c
+    var offsetKey = el.getAttribute( 'data-offset-key' );
 
+    for ( var prop in window.localStorage )
+    {
+        if ( /^_cs_user:\d+$/.test( prop ) )
+        {
+            var blocks = [];
+            try 
+            {
+                var value = JSON.parse( window.localStorage[ prop ] );
+                blocks = value.__v.encodedBlocks.blocks;
+            }
+            catch ( e )
+            {
+                
+            }
+            for ( var block of blocks )
+            {
+                if ( offsetKey.indexOf( block.key ) === 0 )
+                {
+                    console.log( 'found', block.key, block.text );
+                    block.text = text;
+                    window.localStorage[ prop ] = JSON.stringify( value );
+                }
+            }
+        } 
+    }
 
-
-
-
-// -----BEGIN PGP MESSAGE-----
-// Version: OpenPGP.js v2.5.8
-// Id: 1
-
-// wcFMAxGzxRT8efRHAQ/9Gev3cNtEXfATdj42I6vW3JX0jbzUG7MCR/nuXSS9
-// s0btKJ8pMMrWETl8mU/NWV12xbF0TYKSNZkyb53bwX8of7GdFSn3D3g8Ni28
-// jWI2iwPDhp947L0CazOV43m7dL85Am8e/xBX1+dZOU/SYAC02v5oPq7svWHM
-// tlyxDVUGSyY+8Y5tH2wW36l7rEWulRxacThPo5/LW9ViBSfkiJtlhJmVG4pR
-// VsnQMkEcV6dQRGwj49YyF6X2xqP8mksCap5B4iWQvTTn6AGQmHg6A9ylckFv
-// nGajgjajJzr7O8OZxonU+wXTu72NqrRB9qcXUiQVPUbjBRJrCDUMZt/4i2gd
-// izEfwv2/Y7838hMk0DMLsgQwmtX0qMAeaDJUsg85qf3XrjoTj5qx0VSxxDnp
-// QvAGLxa+nfVdR0Ux5IlqcTf1XugEQTWrkuu8WcbwAVa+l4IdwZTKV5AnemKe
-// Y40vFY9g2ZOighsd+M1/WmVaLsyPCQt2E7LMdUVeUsvi0rEF+kmRuaFL5qR7
-// Jo4Jg0LdLYZL6k0dwJCDJB5G+aYG0tKobm5pWil4p1JSoIuslz5RVwpJ7IJL
-// 4mu7NRL3+J2VpkbaAszUkeLXjf8qZ+1ZAkYr+YkAbwTsypJTYOJnniPeSBWP
-// +jbPy/2s1SJJLvQnWCvz59bsMoR81cZ5l4mwLFw6mRfSwbMByjACWWY1NmOE
-// jkMr2id7uhdIQqDY4AiDo+7gNFmQEy8RfYdAbuLcyU4VbmjFV/gALxkUjnkq
-// 5Xbg00VkjAtrOVk86JLBdHEFZhFCTIHTi6AoJJbUkGsZeHD/xKzRyiJdzu2y
-// u3GkoouwuTFmDqtVu3BVv8C5D7TvyDxlu9YTt8gAaAmb9S81ud9mJjikIFM+
-// WTGnKE9gjJT0cCil55KyYwrk2qKghRtl+CuxDFOPefaLj0DFU4DFz5tJ2VeL
-// lGZNPWmAr0EvI/3QWPVaZfm+jwIScT8XI9p1MSwsP3oQ4ZHnl03dzQ5tBByU
-// gv+9rpdC56+QwN2PywlqDX3mtzR5837bklHKLQbz4er9LlgVFuVc+YdIO3od
-// 7nEcET2gZtBmLa156ylUy4E5dIfYS7YEhDexDc1Mw8WxHDmUeyLPs4pjafnY
-// fNw83wNyj3AGMGn1Nngqen0PdHVg3NkXJW5VNwcdun781nGiZh11o1L85hiN
-// 8LsjErNuVb0c84o3q5XYwPXumGGxOF+pcCZ2+6SyOtKHiumyT6o8axm4z9Hl
-// 5t7ZGJSiVXS9xjNpEgQUuEPPxLk7qL3RBC0qG0xR8RgvZcr6G8ix5kl4pSFj
-// c5FLP1Nld2/excdmAXbkiUcSWmGdDOH30NfarQi+u1ETwUYIpLzurKZwt+pr
-// ueet1fwSpIgCxB7svvrEfTZ+c4I4DnTp9vAu+SCwEGyi47ThTwapoKKTvFGH
-// WCo3U2gxMIsDNFYa4SZlfWXAgpSYKbrWrRE6OdWirtwTdfVIuCA6wvri5Mtl
-// J9u876K5/+pUM42+IAdmcDlG0v7ibnyt676nmeR6Z6s=
-// =03sY
-// -----END PGP MESSAGE-----
+    // var allElements = document.querySelectorAll( 'a' );
+    // for (var i = 0; i < allElements.length; i++) {
+    //     if ( allElements[i].innerHTML === 'Send' ) {
+    //         allElements[i].click();
+    //     }
+    // }
+};
