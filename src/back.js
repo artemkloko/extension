31e46fc36ffef29df4a46c86615e234d1da8641e
Artem Kloko <artemkloko@gmail.com>
2017-09-12 13:35:45 +0200

    prog

--- src/back.js
+++ src/back.js
@@ -1,55 +1,58 @@
-console.log("back.js started");
+console.log( "back.js started from", window.location.href );
 
 
 // A function to use as callback
-function doStuffWithDom(domContent) {
-    console.log('I received the following DOM content:\n' );
+function doStuffWithDom( domContent )
+{
+    console.log( 'I received the following DOM content:\n' );
 }
 
 var sendMessage = function ( value, callback )
 {
-    chrome.tabs.getCurrent(function(tab){
-        chrome.tabs.sendMessage(tab.id, value, callback);
-    });
+    chrome.tabs.getCurrent( function ( tab )
+    {
+        chrome.tabs.sendMessage( tab.id, value, callback );
+    } );
 };
 
 var loaded = function ()
 {
     // console.log(window.location);
 
-    // set click 
-    var myEl = document.getElementById('send');
+    // set click
+    var myEl = document.getElementById( 'send' );
     if ( myEl !== null )
     {
-        myEl.addEventListener('click', function ()
+        myEl.addEventListener( 'click', function ()
         {
-            sendMessage( 
+            sendMessage(
             {
-                encrypt : document.getElementById('text').value 
+                encrypt: document.getElementById( 'text' ).value
             } );
-        }, false);
+        }, false );
     }
 
     // try to decrypt message
     var search = window.location.search;
     var searchParts = search.replace( /^\?/, '' ).split( '&' );
-    var params = searchParts.reduce( function ( acc, searchPart ) 
+    var params = searchParts.reduce( function ( acc, searchPart )
     {
         var split = searchPart.split( '=' );
         acc[ split[ 0 ] ] = split[ 1 ];
         return acc;
-    }, {} );
+    },
+    {} );
 
     if ( typeof params.message === 'string' )
     {
-        sendMessage( 
+        sendMessage(
         {
-            decrypt : params.message
-        }, function ( decrypted ) 
+            decrypt: params.message
+        }, function ( decrypted )
         {
             document.getElementsByTagName( 'body' )[ 0 ].innerHTML = decrypted;
         } );
     }
 };
 
-document.addEventListener('DOMContentLoaded', loaded, false);
+document.addEventListener( 'DOMContentLoaded', loaded, false );
--- src/content.js
+++ src/content.js
@@ -1,7 +1,4 @@
-
-
-var interval = setInterval( function ()
-{
+var interval = setInterval( function () {
 
 }, 1000 );
 
@@ -12,50 +9,54 @@ var textInput;
 
 var passphrase = 'super long and hard to guess secret';
 
-var inputFormSrcUrl = chrome.runtime.getURL('templates/input-form.html');
-var messageSrcUrl = chrome.runtime.getURL('templates/message.html');
+var inputFormSrcUrl = chrome.runtime.getURL( 'templates/input-form.html' );
+var messageSrcUrl = chrome.runtime.getURL( 'templates/message.html' );
 
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
+var observer = new MutationObserver( function ( mutations )
+{
+    mutations.forEach( function ( mutation )
+    {
 
-    // console.log(mutation);
+        // console.log(mutation);
 
-    if (!mutation.addedNodes) return;
+        if ( !mutation.addedNodes ) return;
 
-    for (var i = 0; i < mutation.addedNodes.length; i++) {
-        // do things to your newly added nodes here
-        var node = mutation.addedNodes[i];
-
-
-        if ( typeof textInputC === 'undefined' )
+        for ( var i = 0; i < mutation.addedNodes.length; i++ )
         {
-            var sibling = document.querySelector( 'div[class~="_kmc"]' );
-            if ( sibling !== null )
+            // do things to your newly added nodes here
+            var node = mutation.addedNodes[ i ];
+
+
+            if ( typeof textInputC === 'undefined' )
             {
-                textInputC = true;
-                console.log('created input-form.html iframe');
-                var parent = sibling.parentNode;
-                textInput = document.createElement( 'iframe' );
-                textInput.src = inputFormSrcUrl;
-                textInput.setAttribute( 'width', '300px' );
-                textInput.setAttribute( 'height', '50px' );
-                parent.insertBefore( textInput, sibling );
+                var sibling = document.querySelector( 'div[class~="_kmc"]' );
+                if ( sibling !== null )
+                {
+                    textInputC = true;
+                    console.log( 'created input-form.html iframe' );
+                    var parent = sibling.parentNode;
+                    textInput = document.createElement( 'iframe' );
+                    textInput.src = inputFormSrcUrl;
+                    textInput.setAttribute( 'width', '300px' );
+                    textInput.setAttribute( 'height', '50px' );
+                    parent.insertBefore( textInput, sibling );
+                }
+            }
+
+            if ( /class=("|')_aok("|')/.test( node.innerHTML ) )
+            {
+                // console.log(node.innerHTML);
+                var regexp = />(-----BEGIN PGP MESSAGE-----[^-]*-----END PGP MESSAGE-----)</g;
+                var iframe = '<iframe src="' + messageSrcUrl + '?message=$1" width="300px" height="50px"></iframe>';
+                node.innerHTML = node.innerHTML.replace( regexp, '>' + iframe + '<' );
             }
         }
-
-        if ( /class=("|')_aok("|')/.test( node.innerHTML ) )
-        {
-            // console.log(node.innerHTML);
-            var regexp = />(-----BEGIN PGP MESSAGE-----[^-]*-----END PGP MESSAGE-----)</g;
-            var iframe = '<iframe src="' + messageSrcUrl + '?message=$1" width="300px" height="50px"></iframe>';
-            node.innerHTML = node.innerHTML.replace( regexp, '>' + iframe + '<' );
-        }
-    }
-  });
-});
+    } );
+} );
 
 
-observer.observe( document.body, {
+observer.observe( document.body,
+{
     childList: true,
     subtree: true,
     attributes: false,
@@ -64,79 +65,82 @@ observer.observe( document.body, {
 
 var encrypt = function ( decrypted, callback )
 {
-    console.log( 'trying to encrypt', decrypted);
+    console.log( 'trying to encrypt', decrypted );
 
-    var privKeyObj = openpgp.key.readArmored(thisPerson.privkey).keys[0];
-    privKeyObj.decrypt(passphrase);
+    var privKeyObj = openpgp.key.readArmored( thisPerson.privkey ).keys[ 0 ];
+    privKeyObj.decrypt( passphrase );
 
     options = {
-        data: decrypted,                             // input as String (or Uint8Array)
-        publicKeys: openpgp.key.readArmored(keys.alice.pubkey).keys,  // for encryption
+        data: decrypted, // input as String (or Uint8Array)
+        publicKeys: openpgp.key.readArmored( keys.alice.pubkey ).keys, // for encryption
         privateKeys: privKeyObj // for signing (optional)
     };
 
-    openpgp.encrypt(options).then(function(ciphertext) {
-        encrypted = ciphertext.data; 
+    openpgp.encrypt( options ).then( function ( ciphertext )
+    {
+        encrypted = ciphertext.data;
         // console.log( encrypted );
         callback( encrypted );
-    });
+    } );
 };
 
 var decrypt = function ( encrypted, callback )
 {
+    console.log( 'trying to decrypt', encrypted );
 
-                     console.log( 'trying to decrypt', encrypted );
+    var privKeyObj = openpgp.key.readArmored( thisPerson.privkey ).keys[ 0 ];
+    privKeyObj.decrypt( passphrase );
 
-                    var privKeyObj = openpgp.key.readArmored(thisPerson.privkey).keys[0];
-                    privKeyObj.decrypt(passphrase);
+    var pubkeys = [];
+    for ( var person in keys )
+    {
+        pubkeys.push( keys[ person ].privkey );
+    }
 
-                    var pubkeys = keys.map( function ( person )
-                    {
-                        return person.privkey;
-                    } );
+    try
+    {
+        options = {
+            message: openpgp.message.readArmored( encrypted ), // parse armored message
+            publicKeys: openpgp.key.readArmored( pubkeys ).keys, // for verification (optional)
+            privateKey: privKeyObj // for decryption
+        };
 
-                    try
-                    {
-                        options = {
-                            message: openpgp.message.readArmored(encrypted),     // parse armored message
-                            publicKeys: openpgp.key.readArmored(pubkeys).keys,    // for verification (optional)
-                            privateKey: privKeyObj // for decryption
-                        };
-
-                        openpgp.decrypt(options).then(function(plaintext) {
-                            var decrypted = plaintext.data; // 'Hello, World!'
-                            console.log( 'decrypted', decrypted );
-                            callback( decrypted );
-                        });
-                    }
-                    catch ( e )
-                    {
-                        console.log( e );
-                    }
+        openpgp.decrypt( options ).then( function ( plaintext )
+        {
+            var decrypted = plaintext.data; // 'Hello, World!'
+            console.log( 'decrypted', decrypted );
+            callback( decrypted );
+        } );
+    }
+    catch ( e )
+    {
+        // console.log( e );
+    }
 };
 
 // Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) 
+chrome.runtime.onMessage.addListener( function ( msg, sender, sendResponse )
 {
     if ( typeof msg.encrypt === 'string' )
     {
-        encrypt( msg.encrypt, function ( encrypted ) 
+        encrypt( msg.encrypt, function ( encrypted )
         {
             post( encrypted );
         } );
-    } 
+    }
     else if ( typeof msg.decrypt === 'string' )
     {
-        decrypt( msg.decrypt, function ( decrypted ) 
+        decrypt( msg.decrypt, function ( decrypted )
         {
-            sendResponse(decrypted);
+            sendResponse( decrypted );
         } );
-    }  
-});
+    }
+} );
 
 
 var post = function ( text )
-{c
+{
+    var el = document.querySelector('div[contenteditable="true"] div[data-offset-key]');
     var offsetKey = el.getAttribute( 'data-offset-key' );
 
     for ( var prop in window.localStorage )
@@ -144,14 +148,14 @@ var post = function ( text )
         if ( /^_cs_user:\d+$/.test( prop ) )
         {
             var blocks = [];
-            try 
+            try
             {
                 var value = JSON.parse( window.localStorage[ prop ] );
                 blocks = value.__v.encodedBlocks.blocks;
             }
             catch ( e )
             {
-                
+
             }
             for ( var block of blocks )
             {
@@ -162,7 +166,7 @@ var post = function ( text )
                     window.localStorage[ prop ] = JSON.stringify( value );
                 }
             }
-        } 
+        }
     }
 
     // var allElements = document.querySelectorAll( 'a' );
